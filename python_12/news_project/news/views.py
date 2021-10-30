274704023e37f4185f0bd4be05f9094098bdfecf
hoangxq <holmes.arther@gmail.com>
2021-10-31 01:17:48 +0700

    update data 2

--- python_12/news_project/news/views.py
+++ python_12/news_project/news/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
 from .models import Category, New
+import random
 
 # Create your views here.
 
 def home(request):
     categorys = Category.objects.all()
+    news = New.objects.all()
+    latest_post = []
+    latest_post1 = []
+    top_post = []
+    trending = random.sample(list(news), 6)
+
+    for i in news:
+        if len(top_post) < 5:
+            top_post.append(i)
+
+    for i in range(len(news)-1, -1, -1):
+        if len(latest_post) < 5:
+            latest_post.append(news[i])
+        if len(latest_post1) < 3:
+            latest_post1.append(news[i])
+
+
     context = {
+        "Trending" : trending,
+        "Top_post" : top_post,
+        "Latest_post" : latest_post1,
+        "Latest_post_1" : latest_post[0],
+        "Latest_post_2" : latest_post[1],
+        "Latest_post_3" : latest_post[2],
+        "Latest_post_4" : latest_post[3],
+        "Latest_post_5" : latest_post[4],
         "Categorys" : categorys
     }
     return render(request, 'index.html', context)
@@ -16,9 +42,11 @@ def category(request, id):
     top_post , last_post = [], []
     for i in arr:
         if i.category.id == id :
-            if len(top_post) < 5:
+            if len(top_post) < 4:
                 top_post.append(i)
-            else: last_post.append(i)
+    for i in range(len(arr)-1, -1, -1):
+        if len(last_post) < 3:
+            last_post.append(arr[i])
     context = {
         "Categorys" : categorys,
         "news" : arr,
