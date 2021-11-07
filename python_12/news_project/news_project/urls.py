95198a75094bbb6dadbfd52e092e524dde68288e
luong-td <92680608+luong-td@users.noreply.github.com>
2021-11-07 21:36:12 +0700

    Revert "#1"

--- python_12/news_project/news_project/urls.py
+++ python_12/news_project/news_project/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('',include('django.contrib.auth.urls')),
     #path('', include('')),
 ]
 
